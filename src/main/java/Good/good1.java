d03279980fc2fed09889c878076748ba99f20141
kanon-nyc <57508437+kanon-nyc@users.noreply.github.com>
2021-03-08 18:22:00 -0500

    first commit

---
+++ src/main/java/Good/good1.java
@@ -0,0 +1,10 @@
+package Good;
+
+public class good1 {
+    public void a(){
+    }
+    good1 good = new good1();
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
